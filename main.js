141f89a1ea9254353460502f962cc71ef3de0a89
alli <dmsql2670@naver.com>
2021-11-13 20:25:04 +0900

    커밋

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 /*=============== HOME SWIPER ===============*/
-let homeSwiper = new Swiper(".home-swiper", {
-    spaceBetween: 30,
-    loop: 'true',
-    
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-})
+window.onload= () =>{
+  let homeSwiper = new Swiper(".home-swiper", {
+      spaceBetween: 30,
+      loop: 'true',
+      
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+  })
+  const textarea = document.querySelector("textarea")
+  const button = document.querySelector("button")
+  button.addEventListener("click", (e) => {
+      e.preventDefault()
+      fetch("http://127.0.0.1:5000", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          mode: "no-cors",
+          body: JSON.stringify({ data: textarea.value }),
+      }).then((res) => {
+          console.log(res)
+      })
+  })
+  }
+  
+  
